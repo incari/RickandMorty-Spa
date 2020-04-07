adf891e713b05c5b756bc304acf2fd857103032e
Martin Racana <ing.racana@gmail.com>
2020-04-07 13:39:56 +0200

    App done, need more css

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,16 +1,21 @@
 const Header = () => {
   const view = `
-  <div class='Header-main'>
-    <div class='Header-logo'>
-      <h1>
-        <a href='/'>Rick and Morty SPA JSVanilla</a>
-      </h1>
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="/">
+     Rick and Morty Spa - JS Vanilla
+          </a>
+        </h1>
+      </div>
+      <div class="Header-nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
     </div>
-    <div class='Header-nav'>
-      <a href='#/About/'>About</a>
-    </div>
-  </div>
-`;
+  `;
   return view;
 };
+
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Permitir traer path. Acceder dentro de la carpte no importa el lugar donde se encuentre
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -27,6 +28,12 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/styles/styles.css',
+        to: ''
+      }
+    ])
   ]
 };
